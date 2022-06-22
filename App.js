c277ceebfaac4d4b316d92c1bb055f519b37cc31
MajorReact <majorsam001@gmail.com>
2022-06-22 10:16:54 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to maybitz.xyz</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
